462967e4f2df580ed029cd3079dd5db7e650a14a
Hunter <h.dyar@chatham.edu>
2022-11-10 12:38:52 -0500

    Basic UI for win/lose

---
+++ Assets/Scripts/Management/OnVictoryBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Pong
+{
+	/// <summary>
+	/// Base class for easier "on player won" "on player lost" listening components
+	/// </summary>
+	public abstract class OnVictoryBase : MonoBehaviour
+	{
+		[SerializeField] protected PlayerData _winningPlayer;
+
+		private void OnEnable()
+		{
+			PongGameManager.OnPlayerWon += OnPlayerWon;
+		}
+
+		private void OnDisable()
+		{
+			PongGameManager.OnPlayerWon -= OnPlayerWon;
+		}
+
+		private void OnPlayerWon(PlayerData player)
+		{
+			if (player == _winningPlayer)
+			{
+				//we won!
+				OnMyPlayerWon();
+			}
+			else
+			{
+				//we lost!
+				OnMyPlayerLost();
+			}
+		}
+
+		protected virtual void OnMyPlayerWon()
+		{
+			//
+		}
+
+		protected virtual void OnMyPlayerLost()
+		{
+			//
+		}
+	}
+}
---
+++ Assets/Scripts/UI/UIVictoryText.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+
+namespace Pong.UI
+{
+	public class UIVictoryText : OnVictoryBase
+	{
+		private TMP_Text _text;
+
+		private void Awake()
+		{
+			_text = GetComponent<TMP_Text>();
+			_text.color = _winningPlayer.Color;
+			_text.enabled = false;
+		}
+
+		protected override void OnMyPlayerWon()
+		{
+			_text.enabled = true;
+			_text.text = "Victory!";
+		}
+
+		protected override void OnMyPlayerLost()
+		{
+			_text.enabled = true;
+			_text.text = "Defeat!";
+		}
+	}
+}
